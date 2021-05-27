93ca6a75bdd5bee70004b46eb17c23dd36da433b
Daniel Washington <danielvertigo@hotmail.com>
2021-05-27 11:47:04 -0300

    Files

--- HittingTheJackpot/ViewController.swift
+++ HittingTheJackpot/ViewController.swift
@@ -5,6 +5,23 @@
 //  Created by Daniel Washington Ignacio on 27/05/21.
 //
 
+
+/*
+ Create a function that takes in an array (slot machine outcome) and returns true if all elements in the array are identical, and false otherwise. The array will contain 4 elements.
+
+ Examples
+
+ testJackpot(["@", "@", "@", "@"]) ➞ true
+
+ testJackpot(["abc", "abc", "abc", "abc"]) ➞ true
+
+ testJackpot(["SS", "SS", "SS", "SS"]) ➞ true
+
+ testJackpot(["&&", "&", "&&&", "&&&&"]) ➞ false
+
+ testJackpot(["SS", "SS", "SS", "Ss"]) ➞ false
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +29,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(testJackpot(["@", "@", "@", "@"]) )
+        print( testJackpot(["abc", "abc", "abc", "abc"]))
+        print(testJackpot(["SS", "SS", "SS", "SS"]))
+        print(testJackpot(["&&", "&", "&&&", "&&&&"]))
+        print(testJackpot(["SS", "SS", "SS", "Ss"]))
     }
-
+    
+    func testJackpot(_ result: [String]) -> Bool {
+        let firstElement = result.first
+               for element in result {
+                   if element != firstElement {
+                       return false
+                   }
+               }
+           return true
+    }
 
 }
 
